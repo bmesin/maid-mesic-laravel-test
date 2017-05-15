e04aa2bb0e1224823b7a3d7f1388956c79d78944
Meshich <maid91@hotmail.com>
2017-05-15 15:00:24 +0200

    #Maid-Mesic: Laravel Test - Destination Resolver

--- app/Services/Destination/ResolveDestination.php
+++ app/Services/Destination/ResolveDestination.php
@@ -13,7 +13,29 @@ class ResolveDestination implements ResolveDestinationInterface
     public function resolve($input = [])
     {
         // TODO: Find destination by cid
-        // TODO: populate tokens {cid}, {aid}, {sid}, {date}, {rand}
-        return "TODO: Result";
+
+        $campaignsJson = str_replace('\\', '/',storage_path('/data/campaigns.json'));
+        $campaigns = json_decode(file_get_contents($campaignsJson), true);
+
+        // check is campaign selected
+        if ($input["cid"]) {
+            // check is selected campaign in array
+            $objectKey = array_search($input["cid"], array_column($campaigns, 'id'));
+            if ($objectKey) {
+                $campaign = $campaigns[$objectKey];
+
+                // TODO: populate tokens {cid}, {aid}, {sid}, {date}, {rand}
+                $randomInteger = rand();
+                $destination = str_replace(['{cid}', '{sid}', '{aid}', '{rand}', '{date}'], [$input['cid'], $input['sid'], $input['aid'], $randomInteger, date("Y-m-d")], $campaign["destination"]);
+
+                return $destination;
+
+            } else {
+                return "Something went wrong!";
+            }
+        } else {
+            return "Campaign is not selected!";
+        }
+
     }
 }
