b2a7c82aaee35498a703684c77bda7a17197afbb
Meshich <maid91@hotmail.com>
2017-05-16 13:59:11 +0200

    #Maid-Mesic: Laravel Test - Destination Resolver (search sort)

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -13,12 +13,20 @@ class DestinationController extends Controller
         // TODO: Populate campaigns and affiliates select options from JSON data:
         // Hint: The storage_path() helper function will help you to access the data.
 
+        //This function sort array by key
+        function build_sorter($key) {
+            return function ($a, $b) use ($key) {
+                return strnatcmp($a[$key], $b[$key]);
+            };
+        }
+
         $campaignsJson = str_replace('\\', '/',storage_path('/data/campaigns.json'));
         $campaigns = json_decode(file_get_contents($campaignsJson), true);
+        usort($campaigns, build_sorter('title'));
 
         $affiliatesJson = str_replace('\\', '/',storage_path('/data/affiliates.json')) ;
         $affiliates =  json_decode(file_get_contents($affiliatesJson), true);
-
+        usort($affiliates, build_sorter('name'));
 
         return view('destinations.resolve.form', [
             'campaigns' => $campaigns,
